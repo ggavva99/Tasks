732edecc06a8a2bcfa5f5f843f6790d931dbc244
cjpcloud <35867500+cjpcloud@users.noreply.github.com>
2022-08-17 12:21:23 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,9 +5,11 @@ sleep 600
 aws cloudformation create-stack --stack-name mskcluster --template-body file://mskcluster.yaml
 
 sleep 1500
-echo "Enter Msk clusterArn"
-cluster_arn=`aws kafka list-clusters-v2|grep ClusterArn|awk '{print$2}'|grep /AllProperties|sed -e 's/"//gi'|sed -e 's/,//gi'`
+echo "Enter Cluster Name"
+read clustername
+
+cluster_arn=`aws kafka list-clusters-v2|grep ClusterArn|awk '{print$2}'|grep $clustername|sed -e 's/"//gi'|sed -e 's/,//gi'`
 version=`aws kafka describe-cluster --cluster-arn $cluster_arn|grep CurrentVersion|awk '{print$2}'|sed -e 's/"//gi'|sed -e 's/,//gi'`
 aws kafka update-connectivity --cluster-arn $cluster_arn --current-version $version --connectivity-info '{"PublicAccess": {"Type": "SERVICE_PROVIDED_EIPS"}}'
-aws kafka describe-cluster --cluster-arn arn:aws:kafka:us-east-1:348519781890:cluster/AllProperties/9d1da338-0db0-4dea-8bb6-00f7bd357534-23|grep State
+aws kafka describe-cluster --cluster-arn $cluster_arn|grep State
 
