0151054f6b0d61ce7448219fc0fc0884652f8dc3
cjpcloud <35867500+cjpcloud@users.noreply.github.com>
2022-08-17 14:12:03 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
 
-
-#aws cloudformation create-stack --stack-name vpc --template-body file://vpc.yaml
-#sleep 600
+aws cloudformation create-stack --stack-name vpc --template-body file://vpc.yaml
+sleep 600
 aws cloudformation create-stack --stack-name mskcluster --template-body file://mskcluster.yaml
-
-sleep 1500
-
-cluster_arn=`aws kafka list-clusters-v2|grep ClusterArn|awk '{print$2}'|grep $clustername|sed -e 's/"//gi'|sed -e 's/,//gi'`
-version=`aws kafka describe-cluster --cluster-arn $cluster_arn|grep CurrentVersion|awk '{print$2}'|sed -e 's/"//gi'|sed -e 's/,//gi'`
-aws kafka describe-cluster --cluster-arn $cluster_arn|grep ClusterName
-echo "Enter Cluster Name"
+sleep 2200
+aws kafka list-clusters-v2|grep ClusterName
+echo "Enter Cluster Name starting from / "Example: /Properties""
 read clustername
+cluster_arn=`aws kafka list-clusters-v2|grep ClusterArn|awk '{print$2}'|grep $clustername|sed -e 's/"//gi'|sed -e 's/,//gi'`
+echo "mskcluster arn:" $cluster_arn
+version=`aws kafka describe-cluster --cluster-arn $cluster_arn|grep CurrentVersion|awk '{print$2}'|sed -e 's/"//gi'|sed -e 's/,//gi'`
+echo "mskcluster version:" $version 
+
 aws kafka update-connectivity --cluster-arn $cluster_arn --current-version $version --connectivity-info '{"PublicAccess": {"Type": "SERVICE_PROVIDED_EIPS"}}'
+
 aws kafka describe-cluster --cluster-arn $cluster_arn|grep State
+
 
